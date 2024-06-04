36895a91a531ba3f51b7195e94e0c851d9652b74
daffazuhdi <daffazhdihh@gmail.com>
2024-06-04 19:15:32 +0700

    edit schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('service:reminder')->everyTenMinutes();
+        $schedule->command('service:reminder')->everyMinute();
         // ;
         // ->dailyAt('22:39')->timezone('Asia/Jakarta');
     }
